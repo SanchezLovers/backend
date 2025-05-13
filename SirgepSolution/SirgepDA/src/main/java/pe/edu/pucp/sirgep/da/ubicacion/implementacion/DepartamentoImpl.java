09bc27e8890a399523265f0058092e84c2c47e25
William <a20220453@pucp.edu.pe>
2025-05-12 19:31:18 -0500

    Se implemento DepartamentoDAO y se realizo las pruebas correspondientes correctamente

--- SirgepSolution/SirgepDA/src/main/java/pe/edu/pucp/sirgep/da/ubicacion/implementacion/DepartamentoImpl.java
+++ SirgepSolution/SirgepDA/src/main/java/pe/edu/pucp/sirgep/da/ubicacion/implementacion/DepartamentoImpl.java
@@ -1,84 +1,96 @@
 package pe.edu.pucp.sirgep.da.ubicacion.implementacion;
 
 import pe.edu.pucp.sirgep.da.ubicacion.dao.DepartamentoDAO;
-import pe.edu.pucp.sirgep.dbmanager.DBManager;
 import pe.edu.pucp.sirgep.domain.ubicacion.models.Departamento;
 
-import java.io.IOException;
 import java.sql.*;
-import java.util.ArrayList;
+import pe.edu.pucp.sirgep.da.base.implementacion.BaseImpl;
 
-public class DepartamentoImpl implements DepartamentoDAO {
+public class DepartamentoImpl extends BaseImpl<Departamento> implements DepartamentoDAO {
+
+    
+    public DepartamentoImpl(){
+
+    }
+    
     @Override
-    public void insertar(Departamento departamento) throws SQLException, IOException {
-        String sql = "INSERT INTO Departamento(id_departamento, nombre, activo) VALUES (?, ?, 'A')";
-        try (Connection con = DBManager.getInstance().getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
+    protected String getInsertQuery(){
+        String sql = "INSERT INTO Departamento(id_departamento,nombre,activo) "
+                   + "VALUES (?,?,'A')";
+        return sql;
+    }
+    
+    @Override
+    protected String getSelectByIdQuery(){
+        String sql = "SELECT id_departamento,nombre "
+                + "FROM Departamento "
+                + "WHERE id_departamento=?";
+        return sql;
+    }
+    
+    @Override
+    protected String getSelectAllQuery(){
+        String sql = "SELECT id_departamento,nombre "
+                + "FROM Departamento "
+                + "WHERE activo='A'";
+        return sql;
+    }
+    
+    @Override
+    protected String getUpdateQuery(){
+        String sql = "UPDATE Departamento "
+                   + "SET nombre=? "
+                   + "WHERE id_departamento=?";
+        return sql;
+    }
+    
+    @Override
+    protected String getDeleteLogicoQuery(){
+        String sql = "UPDATE Departamento SET activo='E' WHERE id_departamento=?";
+        return sql;
+    }
+    
+    @Override
+    protected String getDeleteFisicoQuery(){
+        String query = "DELETE FROM Departamento WHERE id_departamento=?";
+        return query;
+    }
+    
+    @Override
+    protected void setInsertParameters(PreparedStatement ps, Departamento departamento){
+        try{
             ps.setInt(1, departamento.getIdDepartamento());
             ps.setString(2, departamento.getNombre());
-            ps.executeUpdate();
-
+        }catch(SQLException e){
+            throw new RuntimeException(e);
         }
     }
-
+    
     @Override
-    public void actualizar(Departamento departamento) throws SQLException, IOException {
-        String sql = "UPDATE Departamento SET nombre=? WHERE id_departamento=?";
-        try (Connection con = DBManager.getInstance().getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
+    protected Departamento createFromResultSet(ResultSet rs){
+         try{
+            Departamento departamento = new Departamento();
+            departamento.setIdDepartamento(rs.getInt("id_departamento"));
+            departamento.setNombre(rs.getString("nombre"));
+            return departamento;
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    protected void setUpdateParameters(PreparedStatement ps, Departamento departamento){
+        try{
             ps.setString(1, departamento.getNombre());
             ps.setInt(2, departamento.getIdDepartamento());
-            ps.executeUpdate();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
         }
     }
-
+    
     @Override
-    public void eliminar(int id) throws SQLException, IOException {
-        String sql = "UPDATE Departamento SET activo='E' WHERE id_departamento=?";
-        try (Connection con = DBManager.getInstance().getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setInt(1, id);
-            ps.executeUpdate();
-        }
+    protected void setId(Departamento departamento, int id){
+        departamento.setIdDepartamento(id);
     }
-
-    @Override
-    public Departamento obtenerPorId(int id) throws SQLException, IOException {
-        String sql = "SELECT * FROM Departamento WHERE id_departamento=?";
-        try (Connection con = DBManager.getInstance().getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setInt(1, id);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    Departamento d = new Departamento();
-                    d.setIdDepartamento(rs.getInt("id_departamento"));
-                    d.setNombre(rs.getString("nombre"));
-                    return d;
-                }
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public ArrayList<Departamento> obtenerTodos() throws SQLException, IOException {
-        ArrayList<Departamento> lista = new ArrayList<>();
-        String sql = "SELECT * FROM Departamento";
-        try (Connection con = DBManager.getInstance().getConnection();
-             Statement st = con.createStatement();
-             ResultSet rs = st.executeQuery(sql)) {
-
-            while (rs.next()) {
-                Departamento d = new Departamento();
-                d.setIdDepartamento(rs.getInt("id_departamento"));
-                d.setNombre(rs.getString("nombre"));
-                lista.add(d);
-            }
-        }
-        return lista;
-    }
+    
 }
