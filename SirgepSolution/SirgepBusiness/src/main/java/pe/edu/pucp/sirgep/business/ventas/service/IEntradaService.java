c8e228adda11ef96af03bb9090adcd50fb86bdd4
Jorge Chamorro <a20206234@pucp.edu.pe>
2025-06-07 17:49:13 -0500

    Incluí EntradaWS.java

--- SirgepSolution/SirgepBusiness/src/main/java/pe/edu/pucp/sirgep/business/ventas/service/IEntradaService.java
+++ SirgepSolution/SirgepBusiness/src/main/java/pe/edu/pucp/sirgep/business/ventas/service/IEntradaService.java
@@ -1,6 +1,10 @@
 package pe.edu.pucp.sirgep.business.ventas.service;
 
 import java.util.List;
+import pe.edu.pucp.sirgep.domain.infraestructura.models.Evento;
+import pe.edu.pucp.sirgep.domain.infraestructura.models.Funcion;
+import pe.edu.pucp.sirgep.domain.ubicacion.models.Distrito;
+import pe.edu.pucp.sirgep.domain.usuarios.models.Comprador;
 import pe.edu.pucp.sirgep.domain.ventas.models.Entrada;
 
 public interface IEntradaService {
@@ -10,4 +14,10 @@ public interface IEntradaService {
     boolean actualizar(Entrada entrada);
     boolean eliminarLogico(int id);
     boolean eliminarFisico(int id);
+    
+    //Metodos adicionales
+    public Comprador buscarCompradorDeEntrada(int idComprador);
+    public Funcion buscarFuncionDeEntrada(int idFuncion);
+    public Evento buscarEventoDeEntrada(int idEntrada);
+    public Distrito buscarDistritoDeEntrada(int idEntrada);
 }
