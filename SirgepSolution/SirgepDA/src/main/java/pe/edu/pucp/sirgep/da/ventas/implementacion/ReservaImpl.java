1142f4ce6b5e1d2b37a60046c53383317fae09f1
Ana <a20220557@pucp.edu.pe>
2025-06-18 05:49:55 -0500

    porsiacaso

--- SirgepSolution/SirgepDA/src/main/java/pe/edu/pucp/sirgep/da/ventas/implementacion/ReservaImpl.java
+++ SirgepSolution/SirgepDA/src/main/java/pe/edu/pucp/sirgep/da/ventas/implementacion/ReservaImpl.java
@@ -98,16 +98,11 @@ public class ReservaImpl extends BaseImpl<Reserva> implements ReservaDAO{
             aux.setHorarioIni(rs.getTime("horario_ini").toLocalTime());
             aux.setHorarioFin(rs.getTime("horario_fin").toLocalTime());
             aux.setFechaReserva(rs.getDate("fecha_reserva"));
-<<<<<<< HEAD
-=======
-            aux.setFechaReserva(rs.getDate("fecha_reserva"));
             
             
             aux.setIniString(aux.getHorarioIni().toString());
             aux.setFinString(aux.getHorarioIni().toString());
             
-            
->>>>>>> ramaAnaG
             esp.setIdEspacio(rs.getInt("Espacio_id_espacio"));
             per.setIdPersona(rs.getInt("Persona_id_persona"));
             
