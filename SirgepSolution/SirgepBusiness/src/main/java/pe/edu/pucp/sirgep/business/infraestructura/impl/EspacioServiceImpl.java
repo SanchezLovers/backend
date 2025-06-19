722dfdbbdddaedf7b7b31cc67d1704a7bc911d23
Jorge Chamorro <a20206234@pucp.edu.pe>
2025-06-18 20:36:04 -0500

    Implementacion de EnvioCorreo.java con metodos para enviar correos

--- SirgepSolution/SirgepBusiness/src/main/java/pe/edu/pucp/sirgep/business/infraestructura/impl/EspacioServiceImpl.java
+++ SirgepSolution/SirgepBusiness/src/main/java/pe/edu/pucp/sirgep/business/infraestructura/impl/EspacioServiceImpl.java
@@ -1,17 +1,22 @@
 package pe.edu.pucp.sirgep.business.infraestructura.impl;
 
 import java.util.List;
+import pe.edu.pucp.sirgep.business.infraestructura.dtos.EnvioCorreo;
 import pe.edu.pucp.sirgep.business.infraestructura.service.IEspacioService;
 import pe.edu.pucp.sirgep.da.infraestructura.dao.EspacioDAO;
 import pe.edu.pucp.sirgep.da.infraestructura.implementacion.EspacioImpl;
+import pe.edu.pucp.sirgep.da.usuarios.dao.CompradorDAO;
+import pe.edu.pucp.sirgep.da.usuarios.implementacion.CompradorImpl;
 import pe.edu.pucp.sirgep.domain.infraestructura.models.Espacio;
+import pe.edu.pucp.sirgep.domain.usuarios.models.Comprador;
 
 public class EspacioServiceImpl implements IEspacioService {
-    
     private final EspacioDAO espacioDAO;
+    private final CompradorDAO compradorDAO;
 
     public EspacioServiceImpl(){
         this.espacioDAO=new EspacioImpl();
+        this.compradorDAO=new CompradorImpl();
     }
     
     @Override
@@ -62,4 +67,26 @@ public class EspacioServiceImpl implements IEspacioService {
         return espacioDAO.buscarPorDistritoCategoria(id, cad);
     }
     
+    //Metodo para el envio de correo a los compradores registrados con el mismo distrito
+    @Override
+    public boolean enviarCorreosCompradoresPorDistritoDeEspacio(Espacio espacio){
+        boolean resultado=false;
+        try{
+            List<String>listaCorreosCompradores=compradorDAO.listarPorDistritoFavorito(espacio.getDistrito().getIdDistrito());
+            if (listaCorreosCompradores != null) {
+                EnvioCorreo correo=new EnvioCorreo();
+                String asunto="";
+                String contenido="";
+                String rutaLogo="";
+                resultado=correo.enviarEmail(listaCorreosCompradores,asunto,contenido,rutaLogo);
+                if (!resultado) {
+                    throw new RuntimeException("No se enviaron los correos");
+                }
+            }
+        }catch(Exception ex){
+            throw new RuntimeException("Error al enviar correos a compradores con el mismo distrito del espacio: "+ ex.getMessage());
+        }finally{
+            return resultado;
+        }
+    }
 }
